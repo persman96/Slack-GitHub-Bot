9476fa570078d696c1e2ab3a12db6f617b3beb3f
Pontus <persman@kth.se>
2022-04-25 20:31:26 +0200

    Slight cleanup

--- slack_github_bot/bot.py
+++ slack_github_bot/bot.py
@@ -18,31 +18,7 @@ slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
 client = slack.WebClient(token=SLACK_TOKEN)
 BOT_ID = client.api_call("auth.test")['user_id']
 
-selected_workflow = ""
-
 CHANNEL_ID = "C03DC8FGADN"
-
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-    if BOT_ID != user_id:
-        client.chat_postMessage(channel=channel_id, text=text)
-
-
-@app.route('/run-workflow', methods=['POST'])
-def run_workflow():
-    data = request.form
-    user_id = data.get('user_id')
-    channel_id = data.get('channel_id')
-
-    if BOT_ID != user_id:
-        client.chat_postMessage(channel=channel_id, text='Working on it!')
-
-    return Response(), 200
-
 
 @app.route('/get_branches', methods=['POST'])
 def get_branches():
